0d3a1a5c2e981e9db5b669b643554fc8b8028507
ManuelDevWeb <manueldeveloper17@gmail.com>
2021-10-13 01:27:24 -0500

    Se crearon los componentes (containers) aplicando Atomic Design

---
+++ src/containers/ProductList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Importando componentes
+import {ProductItem} from '../components/ProductItem';
+
+// Importando estilos
+import '../styles/ProductList.scss';
+
+const ProductList = () => {
+    return (  
+        <section className="main-container">
+			<div className="ProductList">
+				<ProductItem />
+			</div>
+		</section>
+    );
+}
+ 
+export {ProductList};
