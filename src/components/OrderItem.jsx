8cda1f9a927da80b9529c9a40b9f441293fe4ffe
ManuelDevWeb <manueldeveloper17@gmail.com>
2021-10-13 01:16:51 -0500

    Se crearon los componentes (átomos) aplicando Atomic Design

---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Importando estilos
+import '../styles/OrderItem.scss';
+
+// Importando imágenes
+import imgClose from '../assets/icons/icon_close.png';
+
+const OrderItem = () => {
+    return (  
+        <div className="OrderItem">
+			<figure>
+				<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+			</figure>
+			<p>Bike</p>
+			<p>$30,00</p>
+			<img src={imgClose} alt="close" />
+		</div>
+    );
+}
+ 
+export {OrderItem};
---
+++ src/components/ProductInfo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// Importando estilos
+import '../styles/ProductInfo.scss';
+
+// Importando imágenes
+import imgAdd from '../assets/icons/bt_add_to_cart.svg';
+
+const ProductInfo = () => {
+    return (  
+        <>
+			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+			<div className="ProductInfo">
+				<p>$35,00</p>
+				<p>Bike</p>
+				<p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+				<button className="primary-button add-to-cart-button">
+					<img src={imgAdd} alt="add to cart" />
+					Add to cart
+				</button>
+			</div>
+		</>
+    );
+}
+ 
+export {ProductInfo};
